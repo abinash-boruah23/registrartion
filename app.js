846af1051308b1e834e9c97f772e86630a61526f
Abinash Boruah <abinash666@Abinashs-MacBook-Air.local>
2021-12-22 13:45:42 +0530

    first

--- app.js
+++ app.js
@@ -138,6 +138,8 @@ app.get('/secret', isLoggedIn, (req,res)=>{
     res.render('secret');
 })
 
-app.listen(3000,()=>{
-    console.log("server is running at 3000");
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+});
